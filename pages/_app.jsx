dfd635a33c8c6a620dc59fad2c57eb9a84a0c2b8
xinjo21 <xinjo21>
2021-04-07 13:44:10 +0800

    Added CSSReset

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,9 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <CSSReset />
       <Component {...pageProps} />
     </ChakraProvider>
   )
